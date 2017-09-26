32e5782aaa7568192eb22ea663f97c3e26ec2a28
Greg <gmolsenvideo@gmail.com>
2017-09-26 12:32:00 -0400

    more ajax stuff

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,12 +1,41 @@
 $().ready(function() {
 
-$.getJSON("http://date.jsontest.com",
-	function(json) {
-	console.log(json);
-	debugJSONx(json);
-		});
-});
+	function getVendor(id) {
+	$.getJSON(
+			"http://prs.gregorydoud.net/Vendors/Get/" + id,
+			function(vendor) {
+				console.log(vendor);
+				}
+			);
+		}
+	$("button").click(function() {
+		var vendorId = $("#inputId").val();
+		getVendor(vendorId);
+	});
 
-function debugJSONx(json) {
-    console.log(json["time"], json["milliseconds_since_epoch"], json["date"]);
-}
+// $.getJSON("http://date.jsontest.com",
+// 	function(json) {
+// 	console.log(json);
+// 	debugJSONx(json);
+// 		});
+
+// $.getJSON("http://ip.jsontest.com",
+//  	function(ipdata) {
+//  	console.log("My IP is", ipdata["ip"]);
+//  		});
+
+// $.getJSON("http://md5.jsontest.com/?text=911_inside_job",
+//  	function(md5) {
+//  	console.log("md5: ", md5["md5"], "original: ", md5["original"]);
+//  		});
+
+
+// $.getJSON("http://prs.gregorydoud.net/Vendors/Listg")
+
+// console.log("this is after IP");
+
+// function debugJSONx(json) {
+//     console.log(json["time"], json["milliseconds_since_epoch"], json["date"]);
+// }
+
+}); //end ready
